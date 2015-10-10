d376dd3a6848eb5d7be54970111bc471b7b8530d
Kyle Fuller <kyle@fuller.li>
2015-10-10 00:45:46 -0700

    Add a pystartup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,5 +7,6 @@ set -x LSCOLORS Gxfxcxdxbxegedabagacad
 set -x EDITOR vim
 
 set -x PIP_REQUIRE_VIRTUALENV "true"
+set -x PYTHONSTARTUP $HOME/.pystartup
 
 alias home="git --work-tree=$HOME --git-dir=$HOME/.files.git"
