794ff1d20e5561bcfa186fa7195ebe23cb8ca95b
Kyle Fuller <kyle@fuller.li>
2015-10-09 20:39:35 -0700

    [fish] Show $ in red on failure

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,7 @@
 function fish_prompt
-    if test -n "$SSH_TTY"
+    if not test $status -eq 0
+        set_color $fish_color_error
+    else if test -n "$SSH_TTY"
         set_color FF69B4
     else
         set_color $fish_color_cwd
