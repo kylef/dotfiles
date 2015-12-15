5563e08146ae904a9d7cde936f79268d7838accc
Kyle Fuller <kyle@fuller.li>
2015-12-15 21:43:31 +0000

    [fish] Add swiftenv

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,3 +11,10 @@ set -x PIP_REQUIRE_VIRTUALENV "true"
 set -x PYTHONSTARTUP $HOME/.pystartup
 
 alias home="git --work-tree=$HOME --git-dir=$HOME/.files.git"
+
+# swiftenv
+if test -d "$HOME/.swiftenv"
+  setenv SWIFTENV_ROOT "$HOME/.swiftenv"
+  setenv PATH "$SWIFTENV_ROOT/bin" $PATH
+  status --is-interactive; and . (swiftenv init -|psub)
+end
