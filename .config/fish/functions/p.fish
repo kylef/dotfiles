c6d1124cd64a6c83f6c9c3bc92be9bd91b21b767
Kyle Fuller <kyle@fuller.li>
2020-09-23 18:28:03 +0100

    fix(p): make p work with gnu find

--- .config/fish/functions/p.fish
+++ .config/fish/functions/p.fish
@@ -1,4 +1,4 @@
 function p
   # Jump into a selected project from `~/Projects`
-  cd ~/Projects/(find ~/Projects -type d -maxdepth 2 -depth 2 -print | cut -d '/' -f5,6 | fzf -1 -q "$argv[1]")
+  cd ~/Projects/(find ~/Projects -maxdepth 2 -type d -print | cut -d '/' -f5,6 | fzf -1 -q "$argv[1]")
 end
