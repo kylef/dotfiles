1d1d35120114e2d22123784c47fff9c5079c2544
Kyle Fuller <kyle@fuller.li>
2022-04-25 11:04:27 +0100

    feat(fish): show `$REGION` in PS1 line

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,9 @@
 function fish_prompt
+    if test -n "$REGION"
+        set_color FF69B4
+        echo -n "[$REGION] "
+    end
+
     if not test $status -eq 0
         set_color $fish_color_error
     else if test -n "$SSH_TTY"
