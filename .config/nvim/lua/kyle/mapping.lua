0ec286d0e62c69f0efade94d78d4e5d04937497f
Kyle Fuller <kyle@fuller.li>
2025-04-03 21:18:42 +0100

    feat(vim): add \K \ca lsp bindings

--- .config/nvim/lua/kyle/mapping.lua
+++ .config/nvim/lua/kyle/mapping.lua
@@ -10,19 +10,31 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local opts = { buffer = args.buf }
 
     vim.keymap.set('n', '<Leader>k', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<Leader>K', vim.lsp.buf.signature_help, opts)
 
     -- refactoring
+    --- rename
     vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
+
+    --- open code action menu
+    vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, opts)
+
+    --- reformat file
     vim.keymap.set('n', '<Leader>m', function()
       vim.lsp.buf.format { async = true }
     end, opts)
 
-    -- goto
+    -- movement
+    --- goto definition
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+
+    --- goto type definition
     vim.keymap.set('n', 'gD', vim.lsp.buf.type_definition, opts)
+
+    --- goto implementation
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
 
-    -- find
+    --- find references
     local telescope = require'telescope.builtin'
     vim.keymap.set('n', '<Leader>r', telescope.lsp_references)
   end,
